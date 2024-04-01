8c123811a3d5a735175a6754c0e992c50109c0b6
charlzyx <charlzyx@qq.com>
2024-04-01 18:08:16 +0800

    fix: 🐛 fix NodeTitleWidget

--- packages/react/src/widgets/NodeTitleWidget/index.tsx
+++ packages/react/src/widgets/NodeTitleWidget/index.tsx
@@ -9,6 +9,7 @@ export const NodeTitleWidget: React.FC<
   React.PropsWithChildren<INodeTitleWidgetProps>
 > = observer((props) => {
   const takeNode = () => {
+    const node = props.node;
     if (node.componentName === "$$ResourceNode$$") {
       return node.children[0];
     }
